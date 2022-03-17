ff494fb974167e32796f471de87783e282364efb
MrGultekin <gultekin.de@gmail.com>
2022-03-17 23:22:45 +0100

    Finished but needs to be Tested

--- app.js
+++ app.js
@@ -17,7 +17,34 @@ document.addEventListener("DOMContentLoaded", event => {
     });
 
     document.querySelector("#btnLearn").addEventListener("click", event => {
-        location.href = "https://frontendmasters.com";
+        location.href = "https://github.com/MrGultekin";
+    })
+
+    let bipEvent = null;
+    window.addEventListener("beforeinstallprompt", event => {
+        event.preventDefault();
+        bipEvent = event;
+    })
+
+    document.querySelector("#btnInstall").addEventListener("click", event => {
+        if (bipEvent) {
+            bipEvent.prompt();
+        } else {
+            // incompatible browser, your PWA is not passing the criteria, the user has already installed the PWA
+            //TODO: show the user information on how to install the app
+            alert("To install the app look for Add to Homescreen or Install in your browser's menu");
+        }
+    })
+
+    document.querySelector("#btnShare").addEventListener("click", event => {
+        let notesString = "";
+        for (let note of notes) {
+            notesString += note + " | "
+        }
+        navigator.share({
+            title: "Jot Masters",
+            text: notesString
+        })
     })
 })
 
